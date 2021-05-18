b4b522337b02dea90337d3a7437aa909952d9b53
FcoJavierGlez <FJGS_86@hotmail.com>
2021-05-18 14:39:08 +0200

    Updated. Fixed DOMContentLoaded with jQuery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(
-    function(){
+    () => {
         $(".twentytwenty-container").twentytwenty(
             {
                 //default_offset_pct: 0.7, // How much of the before image is visible when the page loads
@@ -12,10 +12,6 @@ $(
                 click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
             }
         );
-    }
-);
-$(
-    function(){
         $("#circus #coliseum").twentytwenty();
     }
 );
